350cde8f700ab6feba957abe869281cc5868f48e
jasonluyk <jasonluyk1@gmail.com>
2025-10-01 15:05:49 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-  console.log(`PDFs served from: ${pdfsFolder}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
